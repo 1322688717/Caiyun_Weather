d27f190e12ba6155521441326100d4f78eeb1cb0
David <1322688717@qq.com>
2021-07-06 16:47:47 +0800

    第三次上传

--- Caiyun_75/app/src/main/java/com/example/caiyun_75/confit/Confit.java
+++
@@ -1,6 +0,0 @@
-package com.example.caiyun_75.confit;
-
-public class Confit {
-    public static final String prediction_url = "https://api.caiyunapp.com/v2.5/7KlicJFJqOmm3iIn/114.31,30.52/weather.json?alert=true";
-    public static final String real_url = "https://api.caiyunapp.com/v2.5/7KlicJFJqOmm3iIn/114.31,30.52/weather.json?alert=true";
-}
